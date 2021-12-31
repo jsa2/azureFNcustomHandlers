7dff22caf2aa276f50f445979da2972e4ddaede6
Your Name <you@example.com>
2021-12-31 10:02:31 +0200

    Update users handler

--- azapp/app.js
+++ azapp/app.js
@@ -16,6 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/proxy', require('./routes/stream'));
 app.get('/', usersRouter);
+app.get('/users', require('./routes/users'));
 
 app.listen(port,() => {
     console.log('runnings12', port)
